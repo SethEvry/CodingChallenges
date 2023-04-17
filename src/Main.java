63014ca01e0f028707ef35ca5872f702838173cc
SethEvry <seth.evry@gmail.com>
2023-04-17 11:37:46 -0400

    4-17 challenge using functional interface

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import challenges.Poll;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,8 +12,19 @@ public class Main {
 //
 //        System.out.println(poll.tallyVotes());
 
-        System.out.println(Arrays.toString(ArrayOfMultiples.getArray(5, 10)));
+//        System.out.println(Arrays.toString(ArrayOfMultiples.getArray(5, 10)));
 
+
+        //Using functional interface since there are no functions in java
+        interface Multiples{
+            int[] calculate(int num, int length);
+        }
+
+        Multiples arr = (int num, int length) -> {
+            return IntStream.rangeClosed(1, length).map(number -> number*num).toArray();
+        };
+
+        System.out.println(Arrays.toString(arr.calculate(5, 10)));
 
     }
 }
